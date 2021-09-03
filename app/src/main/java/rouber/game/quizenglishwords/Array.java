e8948aa2417eb1816dbbc06d8eb0f41689e337b1
RouberR <Rouber1998@gmail.com>
2021-09-03 23:30:13 +0300

    Initial commit

---
+++ app/src/main/java/rouber/game/quizenglishwords/Array.java
@@ -0,0 +1,123 @@
+package rouber.game.quizenglishwords;
+
+public class Array {
+
+    //Массив для первого уровня
+    final int[] images1 = {
+            R.drawable.bear,
+            R.drawable.cat,
+            R.drawable.cow,
+            R.drawable.dog,
+            R.drawable.duck,
+            R.drawable.elephant,
+            R.drawable.fish,
+            R.drawable.fox,
+            R.drawable.frog,
+            R.drawable.hen,
+            R.drawable.monkey,
+            R.drawable.mouse,
+            R.drawable.pig,
+            R.drawable.rabbit,
+            R.drawable.wolf
+    };
+
+    final int[] engPower1= {
+            R.string.level1text1Eng,
+            R.string.level1text2Eng,
+            R.string.level1text3Eng,
+            R.string.level1text4Eng,
+            R.string.level1text5Eng,
+            R.string.level1text6Eng,
+            R.string.level1text7Eng,
+            R.string.level1text8Eng,
+            R.string.level1text9Eng,
+            R.string.level1text10Eng,
+            R.string.level1text11Eng,
+            R.string.level1text12Eng,
+            R.string.level1text13Eng,
+            R.string.level1text14Eng,
+            R.string.level1text15Eng,
+
+
+    };
+
+    final int[] rusPower1 = {
+            R.string.level1text1,
+            R.string.level1text2,
+            R.string.level1text3,
+            R.string.level1text4,
+            R.string.level1text5,
+            R.string.level1text6,
+            R.string.level1text7,
+            R.string.level1text8,
+            R.string.level1text9,
+            R.string.level1text10,
+            R.string.level1text11,
+            R.string.level1text12,
+            R.string.level1text13,
+            R.string.level1text14,
+            R.string.level1text15,
+
+    };
+
+
+    // Массив для второго уровня
+    final int[] images2 = {
+            R.drawable.bird,
+            R.drawable.camel,
+            R.drawable.chicken,
+            R.drawable.crocodile,
+            R.drawable.deer,
+            R.drawable.giraffe,
+            R.drawable.goat,
+            R.drawable.hamster,
+            R.drawable.hedgehog,
+            R.drawable.horse,
+            R.drawable.lion,
+            R.drawable.ox,
+            R.drawable.sheep,
+            R.drawable.snake,
+            R.drawable.turtle
+    };
+
+    final int[] engPower2= {
+            R.string.level2text1Eng,
+            R.string.level2text2Eng,
+            R.string.level2text3Eng,
+            R.string.level2text4Eng,
+            R.string.level2text5Eng,
+            R.string.level2text6Eng,
+            R.string.level2text7Eng,
+            R.string.level2text8Eng,
+            R.string.level2text9Eng,
+            R.string.level2text10Eng,
+            R.string.level2text11Eng,
+            R.string.level2text12Eng,
+            R.string.level2text13Eng,
+            R.string.level2text14Eng,
+            R.string.level2text15Eng,
+    };
+
+    final int[] rusPower2 = {
+            R.string.level2text1,
+            R.string.level2text2,
+            R.string.level2text3,
+            R.string.level2text4,
+            R.string.level2text5,
+            R.string.level2text6,
+            R.string.level2text7,
+            R.string.level2text8,
+            R.string.level2text9,
+            R.string.level2text10,
+            R.string.level2text11,
+            R.string.level2text12,
+            R.string.level2text13,
+            R.string.level2text14,
+            R.string.level2text15,
+
+    };
+
+
+
+
+}
---
+++ app/src/main/java/rouber/game/quizenglishwords/GameLevels.java
@@ -0,0 +1,80 @@
+package rouber.game.quizenglishwords;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class GameLevels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gamelevels);
+
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button buttonBack = findViewById(R.id.button_back);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(GameLevels.this, MainActivity.class);
+                    startActivity(intent); finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
+
+        //переход на первый уровень
+        TextView textView1 = findViewById(R.id.textView1);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                Intent intent = new Intent(GameLevels.this, Level1.class);
+                startActivity(intent); finish();
+            }catch (Exception e){
+                }
+            }
+        });
+        //переход на 2 уровень
+        TextView textView2 = findViewById(R.id.textView2);
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    Intent intent = new Intent(GameLevels.this, Level2.class);
+                    startActivity(intent); finish();
+                }catch (Exception e){
+                }
+            }
+        });
+
+
+
+
+
+    }
+//Системная кнопка назад
+    @Override
+    public void onBackPressed(){
+        try {
+            Intent intent = new Intent(GameLevels.this, MainActivity.class);
+            startActivity(intent); finish();
+        }catch (Exception e){
+
+        }
+    }
+
+}
